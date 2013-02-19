397f526eff9b030064508539f7bbe343608a63c2
Henry Todd <henry@hjst.org>
2013-02-19 15:13:09 +0000

    Proof-of-concept for loading an sqlite3 db inside a heroku slug.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var sqlite3 = require('sqlite3');
+var dict = new sqlite3.Database('dictionary.db', sqlite3.PEN_READWRITE);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    dict.get("SELECT COUNT(*) AS count FROM dict", function(err, row) {
+      response.send('Dictionary row count: ' + row.count);
+    });
 });
 
 var port = process.env.PORT || 5000;
