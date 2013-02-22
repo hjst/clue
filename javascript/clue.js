936f6e9f465fc06c5c5b576748ead07e7dc7ee28
Henry Todd <henry@hjst.org>
2013-02-22 09:33:59 +0000

    Added support for linking to results. Closes issue #8.

--- javascript/clue.js
+++ javascript/clue.js
@@ -13,26 +13,34 @@ var process_clue_response = function(data) {
   $('form[name=clue]').children(':submit')
     .val('Find')
     .removeAttr('disabled');
-
+  // stash the sanitised pattern in the URL so users can link to the results
+  window.location.hash = '#'+ data[0].pattern;
 };
 
-// bind the clue handler to the form
-$('form[name=clue]').submit(function(e){
-  e.preventDefault();
-  // clear out any previous results and prep the list
-  $('#clue-results').empty();
-  $('#clue-results').append($('<ol>'));
-  // disable the submit button and display the loading text
-  $(this).children(':submit')
-    .val('Finding...')
-    .attr('disabled', true);
-  $('#clue-status').html('Searching for '+ $('input[name=pattern]').val());
+$(document).ready( function() {
+  // bind the clue handler to the form
+  $('form[name=clue]').submit(function(e){
+    e.preventDefault();
+    // clear out any previous results and prep the list
+    $('#clue-results').empty();
+    $('#clue-results').append($('<ol>'));
+    // disable the submit button and display the loading text
+    $(this).children(':submit')
+      .val('Finding...')
+      .attr('disabled', true);
+    $('#clue-status').html('Searching for '+ $('input[name=pattern]').val());
 
-  // make the jsonp call
-  $.ajax({
-    url:$(this).attr('action') + '?' + $(this).serialize(),
-    dataType:'jsonp',
-    success: process_clue_response
+    // make the jsonp call
+    $.ajax({
+      url:$(this).attr('action') + '?' + $(this).serialize(),
+      dataType:'jsonp',
+      success: process_clue_response
+    });
   });
+  // check for a pattern in the URL, auto-submit if found
+  if (window.location.hash) {
+    $('#pattern').val(window.location.hash.substr(1));
+    $('form[name=clue]').submit();
+  }
 });
 
