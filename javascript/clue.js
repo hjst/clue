f9d025a3461fdf1d0bfab96de3f32b796ca791ac
Henry Todd <henry@hjst.org>
2013-02-22 00:49:03 +0000

    Disable the submit button during processing and display status message.

--- javascript/clue.js
+++ javascript/clue.js
@@ -1,7 +1,5 @@
 // define the handler for the server response
 var process_clue_response = function(data) {
-  $('#clue-results').empty();
-  $('#clue-results').append($('<ol>'));
   $.each(data[0].results, function(key, value) {
     console.log(value);
     $('#clue-results ol').append(
@@ -10,12 +8,28 @@ var process_clue_response = function(data) {
       )
     );
   });
+  // we're finished, so reset the button
+  $('#clue-status').html('');
+  $('form[name=clue]').children(':submit')
+    .val('Find')
+    .removeAttr('disabled');
+
 };
 
 // bind the clue handler to the form
 $('form[name=clue]').submit(function(e){
   e.preventDefault();
   console.log($(this).attr('action') + '?' + $(this).serialize());
+  // clear out any previous results and prep the list
+  $('#clue-results').empty();
+  $('#clue-results').append($('<ol>'));
+  // disable the submit button and display the loading text
+  $(this).children(':submit')
+    .val('Finding...')
+    .attr('disabled', true);
+  $('#clue-status').html('Searching for '+ $('input[name=pattern]').val());
+
+  // make the jsonp call
   $.ajax({
     url:$(this).attr('action') + '?' + $(this).serialize(),
     dataType:'jsonp',
